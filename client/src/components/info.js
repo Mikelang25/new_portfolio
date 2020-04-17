fa030ec2217f995fd6cb598bca9d11ef68e9a866
Mike Lang <michaellang2525@yahoo.com>
2020-04-17 08:25:10 -0400

    contact me form correctly submitting, formatting and styling complete

--- client/src/components/info.js
+++ client/src/components/info.js
@@ -8,34 +8,63 @@ function Info() {
             <div className="row">
                 <div className="picture-container col-md-3">
                     <img alt="profile" className="profile" src="/profile.jpg" />
-                    <span>Name: Mike Lang</span><br></br>
-                    <span>Location: Danbury, CT </span><br></br>
-                    <span>Email: Michaellang2525@yahoo.com</span>
+                    <span className="span-info">Name: Michael Lang</span><br></br>
+                    <span className="span-info">Location: Danbury, CT </span><br></br>
+                    <span className="span-info">Email: Michaellang2525@yahoo.com</span>
                 </div>
                 <div className="col-md-5">
                     <div className="row">
                         <div className="col-md-12 about-container">
-                            <h5>About Me</h5>
-                            <p>
+                            <h5 className="head-about">About Me</h5>
+                            <p className="par-about">
                                 My name is Michael Lang and I currently reside in Danbury, CT with my wife and two dogs. I have a Bachelors degree in Finance from
                                 Quinnipiac University, and my Masters in Information Systems from Marist College with a dual concentration in Systems Management and Data Analytics.
-                                While studying at Quinnipiac University, I played two years on the men’s baseball team. 
+                                While studying at Quinnipiac University, I played two years on the men’s baseball team.
                                 In my spare time I enjoy golfing, watching movies, and playing with my dog.
                             </p>
                         </div>
                     </div>
+                    <div class="row">
+                        <div className="col-md-12 skills-container">
+                            <h5>Education</h5>
+                            <div className="row">
+                                <div className="col-md-8">
+                                    <span className="span-school">UConn Full Stack Web Development Bootcamp</span>
+                                </div>
+                                <div className="col-md-4">
+                                    <span className="span-school"> Aug. 2019 - Apr. 2020</span>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-8">
+                                    <span className="span-school">Marist College - Masters Information Systems</span>
+                                </div>
+                                <div className="col-md-4">
+                                    <span className="span-school"> Aug. 2016 - May. 2019</span>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-8">
+                                <span className="span-school">Quinnipiac Univeristy - Bachelors Business Finance</span>
+                                </div>
+                                <div className="col-md-4">
+                                    <span className="span-school"> Aug. 2010 - May. 2014</span>
+                                </div>
+                            </div>                      
+                        </div>
+                    </div>
                     <div className="row">
-                        <div className="col-md-12 about-container">
-                            <h5>Relevent Technical Compentencies</h5>
-                            <p>
-                            HTML5, CSS3, JavaScript, React, jQuery, Bootstrap, Express.js, Node, MongoDB, Mongoose, MySQL, Sequelize, APIs (creation/utilization), AJAX, Git Bash. 
-                            <br></br><br></br>Additionally: Microsoft Access, Excel, Visio, VBA
+                        <div className="col-md-12 skills-container">
+                            <h5 className="head-comp">Relevent Technical Compentencies</h5>
+                            <p className="par-skills">
+                                HTML5, CSS3, JavaScript, React, jQuery, Bootstrap, Express.js, Node, MongoDB, Mongoose, MySQL, Sequelize, APIs (creation/utilization), AJAX, Git Bash.
+                            <br></br>Additionally: Microsoft Access, Excel, Visio, VBA
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-3 job-container">
-                    <h5>Most Recent Position</h5>
+                    <h5 className="head-pos">Most Recent Position</h5>
                     <div className="row">
                         <div className="col-md-11 job">
                             <span>Rockit Solutions</span><br></br>
